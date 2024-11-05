943d3691daa95877c334e1399e403d75ee615168
ASUS.PIESC <ASUS.PIESC@DESKTOP-412668B>
2024-11-05 22:35:33 +0330

    update matrix

--- session1/src/ir/isc/training/CreateMatrix.java
+++ session1/src/ir/isc/training/CreateMatrix.java
@@ -8,14 +8,9 @@ public class CreateMatrix {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 matrix[i][j] = Math.random();
-            }
-        }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
                 System.out.printf("%.2f ", matrix[i][j]);
-
             }
-            System.out.println();
+            System.out.println(" ");
         }
 	}
 }
