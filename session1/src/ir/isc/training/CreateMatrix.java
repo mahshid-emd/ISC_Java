a31da343e692c826bfcee073ece338352f5fa9f3
ASUS.PIESC <ASUS.PIESC@DESKTOP-412668B>
2024-11-05 22:48:49 +0330

    update matrix

--- session1/src/ir/isc/training/CreateMatrix.java
+++ session1/src/ir/isc/training/CreateMatrix.java
@@ -3,12 +3,12 @@ package ir.isc.training;
 public class CreateMatrix {
 	public void printMatrix(int n) {
 
-		double[][] matrix = new double[n][n];
+		float[][] matrix = new float[n][n];
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                matrix[i][j] = Math.random();
-                System.out.printf("%.2f ", matrix[i][j]);
+                matrix[i][j] = (float) Math.random();
+                System.out.printf("%.2f ",matrix[i][j]);
             }
             System.out.println(" ");
         }
