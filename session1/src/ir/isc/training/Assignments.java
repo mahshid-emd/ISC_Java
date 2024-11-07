78944e5d80fd084c0dc66923f7faedd1e3f9801f
ASUS.PIESC <ASUS.PIESC@DESKTOP-412668B>
2024-11-08 02:01:15 +0330

    delete

--- session1/src/ir/isc/training/Assignments.java
+++ session1/src/ir/isc/training/Assignments.java
@@ -41,33 +41,7 @@ public class Assignments {
 		}
 		System.out.println(message);
 		
-		System.out.println("####################Q3####################");
-		int n = 3;
-		CreateMatrix matrix = new CreateMatrix();
-		matrix.printMatrix(n);
-		
-		System.out.println("####################Q4####################");
-		int[] grades = {90, 80, 64, 88, 98, 72, 45, 98, 54};
-		Grades mygrade = new Grades();
-		mygrade.getGrade(grades);
-		
-		System.out.println("####################Q5####################");
-		BigDecimal amount = new BigDecimal("40000");
-		BigDecimal balance = new BigDecimal("80000");
-		String branch = "Valiasr";
-		
-		System.out.println("constructor(balance, branch):");
-		Account account = new Account(balance, branch);
-		account.withdraw(amount);
-		account.print();
-		
-		System.out.println(" ");
-		
-		System.out.println("constructor(balance):");
-		Account account_1 = new Account(balance);
-		account_1.withdraw(amount);
-		account_1.print();
-		
+
 		
 	}
 
