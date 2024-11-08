20ca4dcc685617c86bbfe5907ee2941a0533006f
ASUS.PIESC <ASUS.PIESC@DESKTOP-412668B>
2024-11-08 21:12:59 +0330

    add tamrin 1

---
+++ assignments/src/session2/tamrin1/Teacher.java
@@ -0,0 +1,16 @@
+package session2.tamrin1;
+
+public class Teacher extends Person {
+
+	public Teacher(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void pleaseNotify() {
+		System.out.println("call for notification");
+
+	}
+
+}
